779096009581fddc20be771cea78371f3fb900da
PeterMontez <peterson.montez9@gmail.com>
2023-06-20 11:47:30 -0300

    I literally spent 3 hours and only finished the nav bar

--- LWFront/src/app/app.module.ts
+++ LWFront/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,

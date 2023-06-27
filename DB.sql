e0b3a1e50ff8ffd4b2da45904d1fc3b0ae2a21ff
PeterMontez <peterson.montez9@gmail.com>
2023-06-27 10:38:25 -0300

    database structured

---
+++ DB.sql
@@ -0,0 +1,82 @@
+use master
+go
+
+if exists(select * from sys.databases where name = 'LinkWave')
+    drop database LinkWave
+
+create database LinkWave
+go
+
+use LinkWave
+go
+
+create table Users(
+    User_id int identity primary_key not null,
+    User varchar(30) not null,
+    Email varchar(100) not null,
+    PasswordHash char(64) not null,
+    Salt char(16) not null,
+    Picture varchar(200) not null,
+    BirthDate date not null
+);
+go
+
+create table Forums(
+    Forum_id int identity primary_key not null,
+    Name varchar(40) not null,
+    Description varchar(150) not null,
+    Created_at date not null
+);
+go
+
+create table ForumUser(
+    Association_id int identity primary_key,
+    User_id int,
+    Forum_id int,
+    foreign key (User_id) references Users(User_id),
+    foreign key (Forum_id) references Forums(Forum_id)
+);
+go
+
+create table Posts(
+    Post_id int identity primary_key,
+    Title varchar(50),
+    Content varchar(500),
+    Picture varchar(200),
+    User_id int,
+    Forum_id int,
+    foreign key (User_id) references Users(User_id),
+    foreign key (Forum_id) references Forums(Forum_id),
+);
+go
+
+create table Positions(
+    Position_id int identity primary_key,
+    Name varchar(30),
+    Forum_id int,
+    foreign key (Forum_id) references Forums(Forum_id),
+);
+go
+
+create table Permissions(
+    Permission_id int identity primary_key,
+    Name varchar(50)
+);
+go
+
+create table PosPerm(
+    PosPerm_id int identity primary_key,
+    Permission_id int,
+    Position_id int,
+    foreign key (Permission_id) references Permissions(Permission_id),
+    foreign key (Position_id) references Positions(Position_id)
+);
+
+create table Ratings(
+    Rating_id int identity primary_key,
+    Rating bit,
+    User_id int,
+    Forum_id int,
+    foreign key (User_id) references Users(User_id),
+    foreign key (Forum_id) references Forums(Forum_id)
+);
